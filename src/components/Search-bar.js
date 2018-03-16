59ce53945ffc0e6e7cc548489d301869eb9ff34f
LizbethFP <lizbeth.fp@gmail.com>
2018-03-16 11:28:56 -0500

    añadiendo filtrado en searchbar

--- src/components/Search-bar.js
+++ src/components/Search-bar.js
@@ -1,9 +1,34 @@
-import React from 'react';
+// import React from 'react';
 
-const SearchBar = () => (
-  <div className="search-bar">
-    <input></input>
-  </div>
-)
+// const SearchBar = () => (
+//   <div className="search-bar">
+//     <input></input>
+//   </div>
+// )
 
-export default SearchBar;
+// export default SearchBar;
+
+import React , { Component } from 'react';
+class SearchBar extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {term : ''}
+  }
+
+  handleOnchange = (e) => {
+    console.log(e.target.value)
+    this.setState({ term : e.target.value})
+  }
+
+  render(){
+    return (
+      <div className="search-bar">
+        <input onChange = {this.handleOnchange}/>
+        El valor del input es : {this.state.term}  
+      </div>
+    )
+  }
+}
+
+export default SearchBar
